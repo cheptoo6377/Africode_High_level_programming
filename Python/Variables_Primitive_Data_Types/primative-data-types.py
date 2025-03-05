5f89f3b39f2d92820d1c93f21c52a34a0696c1bd
cheptoo6377 <cheptoodorothy69@gmail.com>
2025-03-05 15:28:11 +0300

    file

---
+++ Python/Variables_Primitive_Data_Types/primative-data-types.py
@@ -0,0 +1,36 @@
+# #Numbers
+
+# #interger
+# age = 12
+# print(type(age))
+# #float
+# height = 1.5
+# print(type(height))
+# sum = age + height
+# print(type(sum))
+#Boolean
+# students = "Mary,Joseph,Enock,Dorothy,Kipngeno,Kirui"
+# # is_student = "Ben" in students
+# is_student = "dorothy" in students
+# print(is_student)
+# is_student = False
+# print(is_student)
+#strings
+# student_name = "Mary"
+# teacher_name = 'Ben'
+
+student_name = "Mary"
+long_string = "Neighbour"
+class_rep = "Rop"
+
+# x = student_name[0]
+# print(x)
+# y = student_name[3]
+# print(y)
+# z = long_string[-1]
+# print(z)
+# print(student_name[-2])
+print(long_string[:4])
+print(long_string[0:2])
+print(long_string[2:4])
+long_string.capitalize
