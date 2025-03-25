64c32d92b746cca4d5483bce9b70cbc7efee8983
cheptoo6377 <cheptoodorothy69@gmail.com>
2025-03-25 10:56:33 +0300

    modified some file

--- Python/Lists/while_loop/break_continue/continue.py
+++ Python/Lists/while_loop/break_continue/continue.py
@@ -9,9 +9,9 @@
 #     if counter==18:
 #         break
 #     print(counter)
-non = 0
-while non <= 10:
-    non += 1 
-    if non \2 == 0:
-        continue
-    print("odd number:", non)
+# non = 0
+# while non <= 10:
+#     non += 1 
+#     if 
+#         continue
+#     print("odd number:", non)
