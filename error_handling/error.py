1b54fcbd579e90ebf0b70bc9b2cfaccc802f8039
cheptoo6377 <cheptoodorothy69@gmail.com>
2025-04-07 15:11:30 +0300

    files

--- error_handling/error.py
+++ error_handling/error.py
@@ -1,19 +1,37 @@
-a = 1
-b =2
-def divide(a, b):
-    return a / b
-results = divide(1, 0)
-# print(results)
-try:
-    result = divide(1, "one")
-except ZeroDivisionError as e:
-    print(e)
-except TypeError:
-    print("invalid input,atleast enter a number")
-except:
-    print("Error: Division by zero")
-else:
-    print("execution was succesful")
-finally:
-    print("i am being executed,no matter what")
+# a = 1
+# b =2
+# def divide(a, b):
+#     return a / b
+# results = divide(1, 0)
+# # print(results)
+# try:
+#     result = divide(1, "one")
+# except ZeroDivisionError as e:
+#     print(e)
+# except TypeError:
+#     print("invalid input,atleast enter a number")
+# except:
+#     print("Error: Division by zero")
+# else:
+#     print("execution was succesful")
+# finally:
+#     print("i am being executed,no matter what")
+# def func():
+# return None
+#indentation error 
+#syntax error - happens when you have written code that python doesnt understand
+# def func:
+#    results None
+# ****exception error happens during execution of a program 
+# print(dorothy )# nameerror
+
+#value error
+# zero division error 
+# file not found error
+#indexerror
+#logical error
+def sum(a,b):
+    return a/b
+results = sum(10,3)
+print(results)
 
